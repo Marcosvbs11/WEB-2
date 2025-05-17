1d2909c0ea930ffdfee99439e68b54c4ae566b7f
Marcosvbs11 <mvbs9@discente.ifpe.edu.br>
2025-05-16 23:34:32 -0300

    atividade03

--- atividade_02/database/migrations/2025_05_16_203804_add_published_year_to_books_table.php
+++ atividade_02/database/migrations/2025_05_16_203804_add_published_year_to_books_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('books', function (Blueprint $table) {
-            $table->integer('published_year')->nullable();
+               $table->dropColumn('published_year');
         });
     }
 
